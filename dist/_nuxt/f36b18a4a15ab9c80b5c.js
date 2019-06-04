2c47d3c2dffb3f61a50972beaaf0109217c7436c
Eric Anderson <beatsforthemind@gmail.com>
2019-06-03 21:04:35 -0500

    first commit

---
+++ dist/_nuxt/f36b18a4a15ab9c80b5c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{270:function(t,e,n){"use strict";n.r(e);var c={},r=n(33),component=Object(r.a)(c,function(){this.$createElement;this._self._c;return this._m(0)},[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("This is the about page")])])}],!1,null,"299de60d",null);e.default=component.exports}}]);
---
+++ pages/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <section>
+    <header class="hero-image">
+      <div class="hero-text">
+        <h1>Merlin Movie House</h1>
+      </div>
+    </header>
+    <main>
+      <h2>A Cinematic Experience</h2>
+      <p>Merlin Movie House is a place where all your film dreams come true. Ride a dragon, fight a jedi, kiss a toad...</p>
+      <hr />
+      <h2>Now Playing</h2>
+      
+      <!--
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          
+          <div v-if="movie">
+            {{ movie.Title }}<br />
+            {{ movie.Year }}<br />
+            {{ movie.Rated }}<br />
+            {{ movie.Released }}<br />
+            {{ movie.Runtime }}<br />
+            {{ movie.Genre }}<br />
+            {{ movie.Director }}<br />
+            {{ movie.Writer }}<br />
+            {{ movie.Actors }}<br />
+            {{ movie.Plot }}<br />
+            {{ movie.Language }}<br />
+            {{ movie.Country }}<br />
+            {{ movie.Awards }}<br />
+            {{ movie.Poster }}<br />
+            {{ movie.Ratings }} ARRAY<br />
+            {{ movie.Metascore }}<br />
+            {{ movie.imdbRating }}<br />
+            {{ movie.imdbVotes }}<br />
+            {{ movie.imdbVotes }}<br />
+          </div>
+
+        </div>
+      </div>
+      -->
+      
+      <div v-if="movies">
+        <div style="display: block;">
+          {{ movies }}
+        </div>
+        
+        <div v-for="item in movies.Search">
+          <div style="display: none;">
+            {{ item }}
+          </div>
+        
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" :alt="item.Title" :src="item.Poster">
+            <div class="card-body">
+              <h5 class="card-title">{{ item.Title }}</h5>
+              <p class="card-text">{{ item.Plot }}</p>
+              <!--
+              <a href="#" class="btn btn-primary">Visit on OMDB</a>
+              -->
+            </div>
+          </div>
+      
+      
+        </div>
+      </div>
+      
+      
+    </main>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      loading: true,
+      // movie: null,
+      // movieTitle: null,
+      movies: null,
+      errored: false,
+    };
+  },
+  mounted () {
+    /*
+    axios.get('http://www.omdbapi.com/?t=her&apikey=29917929')
+      .then(response => (
+        this.movie = response.data,
+        this.movieTitle = response.data.Title
+      ))
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+    .finally(() => this.loading = false);
+    */
+    axios.get('http://www.omdbapi.com/?s=wars&apikey=29917929')
+      .then(response => (
+        this.movies = response.data
+      ))
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+}
+</script>
+
+<style>
+/* Styling for main hero image */
+.hero-image {
+  background-image: url('../assets/header.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom;
+  position: relative;
+  height: 600px;
+  width: 100%;
+}
+
+/* Text laying over hero image */
+.hero-text {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+}
+header h1 {
+  font-family: Limelight, sans-serif;
+  font-size: 5em;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-shadow: 2px 2px black;
+}
+
+main {
+  margin: 2rem 3rem;
+  font-family: Merriweather;
+}
+
+nav {
+  font-family: Merriweather;
+}
+
+/* ##### */
+.card {
+}
+/* ##### */
+</style>
